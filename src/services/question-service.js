3fe0ad14b702a6041a3e5cfb297d7e0375d39379
Selen TOPTAS <selentoptascs@gmail.com>
2021-04-20 19:40:38 -0400

    update server.js

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,4 +1,4 @@
-const QUIZZES_URL = 'https://cs5610-sp21-stoptas-javaserver.herokuapp.com/api/quizzes';
+const QUIZZES_URL = 'https://cs5610-sp21-stoptas-nodeserver.herokuapp.com/api/quizzes';
 const findQuestionsForQuiz = (qid) => {
     return fetch(`${QUIZZES_URL}/${qid}/questions`)
         .then(response => response.json())
